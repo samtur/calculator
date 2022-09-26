d93d108ef3016d873f893c1ecc10a777512b342a
samtur <samturland23@gmail.com>
2022-09-26 20:00:50 +0800

    Started adding functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Query Selectors
 const btn1 = document.querySelector("#btn1");
 const btn2 = document.querySelector("#btn2");
 const btn3 = document.querySelector("#btn3");
@@ -16,12 +17,63 @@ const btnequals = document.querySelector("#btnequals");
 const btnclear = document.querySelector("#btnclear");
 const caldisplay = document.querySelector("#caldisplay");
 
-let num1 = 0;
-let num2 = 0;
+// Variables
+let num1 = null;
+let num2 = null;
+
+// Event Listeners
+btn1.addEventListener("click", () => {
+  caldisplay.textContent += 1;
+});
+btn2.addEventListener("click", () => {
+  caldisplay.textContent += 2;
+});
+btn3.addEventListener("click", () => {
+  caldisplay.textContent += 3;
+});
+btn4.addEventListener("click", () => {
+  caldisplay.textContent += 4;
+});
+btn5.addEventListener("click", () => {
+  caldisplay.textContent += 5;
+});
+btn6.addEventListener("click", () => {
+  caldisplay.textContent += 6;
+});
+btn7.addEventListener("click", () => {
+  caldisplay.textContent += 7;
+});
+btn8.addEventListener("click", () => {
+  caldisplay.textContent += 8;
+});
+btn9.addEventListener("click", () => {
+  caldisplay.textContent += 9;
+});
+btn0.addEventListener("click", () => {
+  caldisplay.textContent += 0;
+});
+btnadd.addEventListener("click", () => {
+  if (num1 === null) {
+    num1 = caldisplay.textContent;
+    caldisplay.textContent = null;
+    console.log(num1);
+  }
+});
+
+btnequals.addEventListener("click", () => {
+  num2 = caldisplay.textContent;
+  caldisplay.textContent = null;
+  console.log(num2);
+  add(num1, num2);
+});
 
 function add(num1, num2) {
   let total = 0;
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  console.log(typeof num1);
   total = num1 + num2;
+  caldisplay.textContent = total;
   return total;
 }
 
@@ -58,12 +110,6 @@ function divide(num1, num2) {
 //     }
 // }
 
-btn1.addEventListener("click", () => {
-  caldisplay.textContent += 1;
-});
-btn2.addEventListener("click", () => {
-  caldisplay.textContent += 2;
-});
 //  CONTINUE MAKING EVENT LISTENERS FOR BUTTONS
 // MAKE A FUNCTION THAT SAVES THE INPUTS AND CALLS AN OPERATION ON THEM
 
