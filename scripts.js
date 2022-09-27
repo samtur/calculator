b0de2345f83a4cf082d24c9f8429cac028764f44
samtur <samturland23@gmail.com>
2022-09-27 17:05:02 +0800

    updated js

--- scripts.js
+++ scripts.js
@@ -16,84 +16,140 @@ const btndivi = document.querySelector("#btndivi");
 const btnequals = document.querySelector("#btnequals");
 const btnclear = document.querySelector("#btnclear");
 const caldisplay = document.querySelector("#caldisplay");
+const totaldisplay = document.querySelector("#totaldisplay");
 
 // Variables
-let num1 = null;
-let num2 = null;
+let num = null;
+let value = [];
 
-// Event Listeners
-btn1.addEventListener("click", () => {
-  caldisplay.textContent += 1;
+// Event Listeners NumBtns
+document.querySelectorAll(".numbtn").forEach((item) => {
+  item.addEventListener("click", (e) => {
+    item === btn1
+      ? (caldisplay.textContent += 1)
+      : item === btn2
+      ? (caldisplay.textContent += 2)
+      : item === btn3
+      ? (caldisplay.textContent += 3)
+      : item === btn4
+      ? (caldisplay.textContent += 4)
+      : item === btn5
+      ? (caldisplay.textContent += 5)
+      : item === btn6
+      ? (caldisplay.textContent += 6)
+      : item === btn7
+      ? (caldisplay.textContent += 7)
+      : item === btn8
+      ? (caldisplay.textContent += 8)
+      : item === btn9
+      ? (caldisplay.textContent += 9)
+      : (caldisplay.textContent += 0);
+  });
 });
-btn2.addEventListener("click", () => {
-  caldisplay.textContent += 2;
-});
-btn3.addEventListener("click", () => {
-  caldisplay.textContent += 3;
-});
-btn4.addEventListener("click", () => {
-  caldisplay.textContent += 4;
-});
-btn5.addEventListener("click", () => {
-  caldisplay.textContent += 5;
-});
-btn6.addEventListener("click", () => {
-  caldisplay.textContent += 6;
-});
-btn7.addEventListener("click", () => {
-  caldisplay.textContent += 7;
-});
-btn8.addEventListener("click", () => {
-  caldisplay.textContent += 8;
-});
-btn9.addEventListener("click", () => {
-  caldisplay.textContent += 9;
-});
-btn0.addEventListener("click", () => {
-  caldisplay.textContent += 0;
-});
+
+// Event Listeners Operator Buttons
 btnadd.addEventListener("click", () => {
-  if (num1 === null) {
-    num1 = caldisplay.textContent;
+  if (num === null) {
+    num = caldisplay.textContent;
+    num = parseInt(num);
+    value.push(num);
     caldisplay.textContent = null;
-    console.log(num1);
+    console.log(value);
+  } else {
+    num = caldisplay.textContent;
+    num = parseInt(num);
+    value.push(num);
+    caldisplay.textContent = null;
+    console.log(value);
+    return add(value);
   }
 });
 
-btnequals.addEventListener("click", () => {
-  num2 = caldisplay.textContent;
-  caldisplay.textContent = null;
-  console.log(num2);
-  add(num1, num2);
+btnsub.addEventListener("click", () => {
+  if (num === null) {
+    num = caldisplay.textContent;
+    num = parseInt(num);
+    value.push(num);
+    caldisplay.textContent = null;
+    console.log(value);
+  } else {
+    num = caldisplay.textContent;
+    num = parseInt(num);
+    value.push(num);
+    caldisplay.textContent = null;
+    console.log(value);
+    return subtract(value);
+  }
 });
 
-function add(num1, num2) {
-  let total = 0;
-  num1 = parseInt(num1);
-  num2 = parseInt(num2);
-  console.log(typeof num1);
-  total = num1 + num2;
-  caldisplay.textContent = total;
-  return total;
+// btnmulti.addEventListener("click", () => {
+//   if (num1 === null) {
+//     num1 = caldisplay.textContent;
+//     caldisplay.textContent = null;
+//     console.log(num1);
+//   }
+//   btnequals.addEventListener("click", () => {
+//     num2 = caldisplay.textContent;
+//     caldisplay.textContent = null;
+//     console.log(num2);
+//     multiply(num1, num2);
+//   });
+// });
+
+// btndivi.addEventListener("click", () => {
+//   if (num1 === null) {
+//     num1 = caldisplay.textContent;
+//     caldisplay.textContent = null;
+//   }
+//   btnequals.addEventListener("click", () => {
+//     num2 = caldisplay.textContent;
+//     caldisplay.textContent = null;
+//     divide(num1, num2);
+//   });
+// });
+
+// Opertaor Functions
+function add(array) {
+  array.reduce((num1, num2) => {
+    total = num1 + num2;
+    return total;
+  });
+  console.log(total);
+  totaldisplay.textContent = total;
+  value = [];
+  value.push(total);
+  console.log(value);
 }
 
-function subtract(num1, num2) {
-  let total = 0;
-  total = num1 - num2;
-  return total;
+function subtract(array) {
+  array.reduce((num1, num2) => {
+    total = num1 - num2;
+    return total;
+  });
+  console.log(total);
+  totaldisplay.textContent = total;
+  value = [];
+  value.push(total);
+  console.log(value);
 }
 
-function multiply(num1, num2) {
-  let total = 0;
-  total = num1 * num2;
-  return total;
-}
+// function multiply(num1, num2) {
+//   let total = 0;
+//   num1 = parseInt(num1);
+//   num2 = parseInt(num2);
+//   total = num1 * num2;
+//   caldisplay.textContent = total;
+//   return total;
+// }
 
-function divide(num1, num2) {
-  let total = 0;
-  total = num1 / num2;
-  return total;
-}
+// function divide(num1, num2) {
+//   let total = 0;
+//   num1 = parseInt(num1);
+//   num2 = parseInt(num2);
+//   total = num1 / num2;
+//   caldisplay.textContent = total;
+//   return total;
+// }
 
 // function operate(num1, num2, operate) {
 //     if(operate === /* add */) {
@@ -110,7 +166,17 @@ function divide(num1, num2) {
 //     }
 // }
 
-//  CONTINUE MAKING EVENT LISTENERS FOR BUTTONS
-// MAKE A FUNCTION THAT SAVES THE INPUTS AND CALLS AN OPERATION ON THEM
-
-// display();
+/* Push num to the total array (num becomes total)
+   Listen for the operation
+   Push num to the total array
+   Perform the operation on total and num in arrray. 
+   Listen for the operation
+   Push num to the total array
+   Perform the operation 
+   Push num to the total array
+   Perform the operation
+   ....
+  If equals is pressed
+  Perform the operation
+  pressed display total from the array.
+     */
